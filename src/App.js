1637cf01ccecd6b18acb5bce8698a1f062acc557
davidjj76 <davidjj76@gmail.com>
2022-03-09 23:12:14 +0100

    Styled components and classnames

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import classNames from 'classnames';
+
+import Button from './components/common/Button';
 import TweetsPage from './components/tweets/TweetsPage';
 
 function App() {
+  const container = true;
   return (
-    <div className="App">
+    // <div className={`App ${container ? 'container' : ''}`}>
+    <div className={classNames('App', { container })}>
       <TweetsPage />
+      <Button variant="primary">Primary</Button>
+      <Button>Normal</Button>
+      <Button disabled>Disabled</Button>
     </div>
   );
 }

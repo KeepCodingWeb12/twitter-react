476f7a1b2984d731d093d32e1a5fe591d48cb780
davidjj76 <davidjj76@gmail.com>
2022-03-14 20:59:14 +0100

    Some links

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,11 @@
+import { Link, NavLink } from 'react-router-dom';
 import classNames from 'classnames';
-import Button from '../common/Button';
 
+import Button from '../common/Button';
 import { ReactComponent as Icon } from '../../assets/twitter.svg';
+import { logout } from '../auth/service';
 
 import './Header.css';
-import { logout } from '../auth/service';
 
 function Header({ className, isLogged, onLogout }) {
   const handleLogoutClick = async () => {
@@ -14,11 +15,28 @@ function Header({ className, isLogged, onLogout }) {
 
   return (
     <header className={classNames('header', className)}>
-      <div className="header-logo">
-        {/* <img src={logo} alt="Twitter-React"></img> */}
-        <Icon width="32" height="32" />
-      </div>
+      <Link to="/">
+        <div className="header-logo">
+          {/* <img src={logo} alt="Twitter-React"></img> */}
+          <Icon width="32" height="32" />
+        </div>
+      </Link>
       <nav className="header-nav">
+        <NavLink
+          to="/tweets/new"
+          // className={({ isActive }) => (isActive ? 'active' : '')}
+          style={({ isActive }) => (isActive ? { color: 'green' } : null)}
+        >
+          New Tweet
+        </NavLink>
+        <NavLink
+          to="/tweets"
+          // className={({ isActive }) => (isActive ? 'active' : '')}
+          style={({ isActive }) => (isActive ? { color: 'green' } : null)}
+          end
+        >
+          See all tweets
+        </NavLink>
         {isLogged ? (
           <Button className="header-button" onClick={handleLogoutClick}>
             Logout
